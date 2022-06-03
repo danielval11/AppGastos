1b74dd7aed7f1c9a369ac6aa91f5f2d70b989e8f
Daniel Valbuena <danielval0708@gmail.com>
2022-06-03 12:04:47 -0300

    segunda modificacion

--- AppGastos/script.js
+++ AppGastos/script.js
@@ -1,4 +1,25 @@
-function convertir (){ let valor1 = document.getElementById("nombre").value
-let resultado = valor1 * operacion
-document.getElementById("cuenta").value = resultado}
+let contar = 0;
+let sumar = 0;
+let cortar = 0;
 
+function convertir(){
+    let nombre = $("#gente").val();
+    let dinero = $("#dinero").val();
+    contar += 1;
+
+    distritube(nombre, dinero, contar);
+
+    let mostrarNombre = `<h4> ${nombre}: </h4>`
+    let mostrarDinero = `<h4> $ ${dinero}: </h4>`
+
+    $("#nuevoNombre").append(mostrarNombre);
+    $("#dolar").append(mostrarDinero);
+}
+
+function distritube(persan, spent, c){
+    sumar += parseInt(spent);
+    $("#resultado").text("$" + sumar);
+
+    cortar = sumar / c;
+    $("#cortar").text("$" + (Math.round(cortar)));
+}
